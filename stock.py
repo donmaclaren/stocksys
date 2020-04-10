775e77c7f68dcfab8759322e6b3a062ec93316c1
donmaclaren <maclarendonald@gmail.com>
2020-04-10 09:30:45 +0100

    Initial Commit

---
+++ stock.py
@@ -0,0 +1,63 @@
+from flask import Flask, Response, render_template, request,g
+import json
+from wtforms import TextField,Form,SubmitField
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'stockman.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+#global conn, cursor
+#conn = sqlite3.connect("stockman.db")
+
+#conn.row_factory = lamda cursor, row: row[0]
+#cursor = conn.cursor()
+
+
+
+
+
+
+class SearchForm(Form):
+    autocomp = TextField('Select Part No', id='parts_autocomplete')
+    submit = SubmitField('Select')
+
+
+
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    cur = get_db().cursor()  
+    pnums = cur.execute("SELECT PART_NO FROM `parts`;").fetchall()
+    COLUMN = 0
+    parts=[elt[COLUMN] for elt in pnums]
+    return Response(json.dumps(parts), mimetype='application/json')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = SearchForm(request.form)
+    if request.method == "POST":
+        dat = form.autocomp.data
+        print(dat)
+        cur = get_db().cursor()
+        res = cur.execute(f"SELECT * FROM `parts` WHERE PART_NO = '{dat}';").fetchone()         
+#        print(res)
+        return render_template("parts01.html", res=res[1:], form=form)
+    return render_template("search.html", form=form)        
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False )
